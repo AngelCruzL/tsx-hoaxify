3b6bcc7ffcbc5ee21adfe897d7305ca1487d45a6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-11-05 15:22:08 -0600

    Add form fields layout test cases

--- src/auth/pages/Signup/Signup.test.tsx
+++ src/auth/pages/Signup/Signup.test.tsx
@@ -7,8 +7,63 @@ describe('Signup', () => {
   describe('Layout', () => {
     it('should render the heading of the signup page', () => {
       render(<SignupPage />);
+      const pageHeading = screen.getByRole('heading', { name: 'Sign Up' });
 
-      expect(screen.getByRole('heading', { name: 'Sign Up' })).toBeDefined();
+      expect(pageHeading).toBeDefined();
+    });
+
+    it('should render the input for display name', () => {
+      render(<SignupPage />);
+      const displayNameInput = screen.getByPlaceholderText('Your display name');
+
+      expect(displayNameInput).toBeDefined();
+    });
+
+    it('should render the input for username', () => {
+      render(<SignupPage />);
+      const usernameInput = screen.getByPlaceholderText('Your username');
+
+      expect(usernameInput).toBeDefined();
+    });
+
+    it('should render the input for password', () => {
+      render(<SignupPage />);
+      const passwordInput = screen.getByPlaceholderText('Your password');
+
+      expect(passwordInput).toBeDefined();
+    });
+
+    it('should render the input for password as password input type', () => {
+      render(<SignupPage />);
+      const passwordInput = screen.getByPlaceholderText('Your password');
+
+      expect(passwordInput.getAttribute('type')).toBe('password');
+    });
+
+    it('should render the input for password confirmation', () => {
+      render(<SignupPage />);
+      const passwordConfirmationInput = screen.getByPlaceholderText(
+        'Confirm your password',
+      );
+
+      expect(passwordConfirmationInput).toBeDefined();
+    });
+
+    it('should render the input for password confirmation as password input type', () => {
+      render(<SignupPage />);
+      const passwordConfirmationInput = screen.getByPlaceholderText(
+        'Confirm your password',
+      );
+
+      expect(passwordConfirmationInput.getAttribute('type')).toBe('password');
+    });
+
+    it('should render the submit button', () => {
+      render(<SignupPage />);
+      const signupButton = screen.getByRole('button', { name: 'Sign Up' });
+
+      expect(signupButton).toBeDefined();
+      expect(signupButton.getAttribute('type')).toBe('submit');
     });
   });
 });
--- src/auth/pages/Signup/Signup.tsx
+++ src/auth/pages/Signup/Signup.tsx
@@ -4,6 +4,26 @@ export const SignupPage: FC = (): ReactElement => {
   return (
     <>
       <h1>Sign Up</h1>
+
+      <div>
+        <input placeholder="Your display name" />
+      </div>
+
+      <div>
+        <input placeholder="Your username" />
+      </div>
+
+      <div>
+        <input placeholder="Your password" type="password" />
+      </div>
+
+      <div>
+        <input placeholder="Confirm your password" type="password" />
+      </div>
+
+      <div>
+        <button type="submit">Sign Up</button>
+      </div>
     </>
   );
 };
